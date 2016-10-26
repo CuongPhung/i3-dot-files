d08d0c055670b2d7b23058bdfcdc2624d98cfb77
nejton <christiannordahl@hotmail.com>
2016-10-26 13:39:34 +0200

    Added installation script. Also functionaltiy for copying over fonts to correct folder

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Make hidden fonts folder in the home directory
+# and copy fonts over to that folder.
+mkdir -p ~/.fonts
+cp Font-Awesome/fonts/fontawesome-webfont.ttf YosemiteSanFranciscoFont/*.ttf ~/.fonts/
