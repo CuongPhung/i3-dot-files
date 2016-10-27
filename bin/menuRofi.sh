a70c7d4774b50d5850d5f55446403a64efa81d0b
Cuong Phung <cuong.phng@gmail.com>
2016-10-27 10:30:37 +0200

    menuRofi, rofi filters out all commands, only .desktop are displayed

--- bin/menuRofi.sh
+++ bin/menuRofi.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rofi -show run -fullscreen -bg "#282828" -fg "#504945" -hlbg "#d65d0e" -font "System San Francisco Display 24" -padding 250
+i3-dmenu-desktop --dmenu='rofi -dmenu -fullscreen -bg "#282828" -fg "#504945" -hlbg "#d65d0e" -font "System San Francisco Display 24" -padding 250'
