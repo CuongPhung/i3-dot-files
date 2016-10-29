3fb0801b3973000c249f8dbab229ca831d303352
nejton <christiannordahl@hotmail.com>
2016-10-29 13:21:37 +0200

    Divided dependencies to section and added compton

--- dependencies.sh
+++ dependencies.sh
@@ -3,3 +3,6 @@ sudo apt-get install i3 i3blocks rofi -y
 
 #For i3 gaps
 sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev dh-autoreconf -y
+
+#compton
+sudo apt-get install compton -y
